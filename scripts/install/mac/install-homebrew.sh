11c84f45fb8ecf6c6563c41660f837c09e539bd9
Alex <alex@Narya.local>
2025-11-28 01:51:37 -0800

    Adding homebrew install script.

---
+++ scripts/install/mac/install-homebrew.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+# This script installs Homebrew on macOS.
+
+echo "Starting Homebrew installation..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
